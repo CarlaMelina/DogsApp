add65690c3026c92e6d27a48b1184d9b77a55ed9
CarlaMelina <cmps03.5@gmail.com>
2023-08-05 22:03:53 -0300

    Agregando cambios y archivos nuevos

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
+
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import { Landing, Home, Detail, Form } from "./views/index"
+import Error404 from './components/Loading/Error404';
+
 
 function App() {
+   
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
-    </div>
+<Switch>
+        <Route exact path='/'><Landing /></Route>
+        <Route exact path='/home'><Home/></Route>
+        <Route exact path='/detail/:idRaza'><Detail/></Route>    
+        <Route exact path='/create'><Form/></Route>
+        <Route path="*"> <Error404 /> </Route>  
+  </Switch>      
+     </div>
   );
-}
+}   
+            
 
 export default App;
